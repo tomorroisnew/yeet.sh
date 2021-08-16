756a7c86c949328ccaa90dd8a561a5bfd5310195
noobexploiter <85917161+noobexploiter@users.noreply.github.com>
2021-08-16 18:06:36 +0800

    Update yeet.sh

--- yeet.sh
+++ yeet.sh
@@ -10,7 +10,7 @@ cd passive
 
 #amass enum --passive -d $1 -o amass.txt & orig
 amass enum -passive -d $1 -o amass.txt -r 8.8.8.8 &
-subfinder -d xsolla.com -all -o subfinder.txt &
+subfinder -d $1 -all -o subfinder.txt &
 assetfinder $1 -subs-only | grep -v '*' | tee assetfinder.txt &
 curl -s https://crt.sh/?q=$1 | grep $1 | cut -d ">" -f2 | cut -d "<" -f 1 | sort -u | tee crt.txt &
 sublist3r -d $1 -o sublister.txt &
@@ -24,7 +24,7 @@ cat passive/* | sort -u > passiveoutput.txt
 altdns -i passiveoutput.txt -o altdns1.txt -w ~/tools/altdns/words.txt
 
 #massdns
-~/tools/massdns/bin/massdns -r ~/tools/massdns/lists/resolvers.txt -t A -o S passiveoutput.txt -w lmao.txt
+~/tools/massdns/bin/massdns -r ~/tools/massdns/lists/resolvers.txt -t A -o S altdns1.txt -w lmao.txt
 #parsemassdns
 sed 's/A.*//' lmao.txt | sed 's/CN.*//' | sed 's/\..$//' | sort -u > massdns1.txt
 
@@ -32,7 +32,7 @@ sed 's/A.*//' lmao.txt | sed 's/CN.*//' | sed 's/\..$//' | sort -u > massdns1.tx
 
 #anadawan
 altdns -i massdns1.txt -o altdns2.txt -w ~/tools/altdns/words.txt
-~/tools/massdns/bin/massdns -r ~/tools/massdns/lists/resolvers.txt -t A -o S passiveoutput.txt -w lmao2.txt
+~/tools/massdns/bin/massdns -r ~/tools/massdns/lists/resolvers.txt -t A -o S altdns2.txt -w lmao2.txt
 sed 's/A.*//' lmao2.txt | sed 's/CN.*//' | sed 's/\..$//' | sort -u > massdns2.txt
 cat massdns2.txt | grep $1 | httpx -title -status-code -o final.txt -location -content-length -no-color
 #rm -rf lmao.txt altdns1.txt
